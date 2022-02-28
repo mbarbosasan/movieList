7c8d767cacfa8194e2a9611fb5c546e19766968e
moisesbarbosa23@gmail.com <moisesbarbosa23@gmail.com>
2022-02-27 23:18:51 -0300

    Refatorando o código

--- script.js
+++ script.js
@@ -2,44 +2,43 @@ const movieImg = document.querySelector(".movie-img");
 const movieTitle = document.querySelector(".movie-name");
 const movieDescription = document.querySelector(".movie-description");
 const watchProviders = document.querySelector(".watch-providers");
+const watchProvidersContainer = document.querySelector(".watch-providers-container")
 const body = document.querySelector("body");
 
 const apiKey = `7163d507a975c5833a02e7ea696637bd`;
 const imgLink = "https://image.tmdb.org/t/p/";
 
-const maxNumber = 20;
+const maxNumber = 19;
 const minNumber = 10;
 const randomNumber = Math.floor(
   Math.random() * (1 + maxNumber - minNumber) + minNumber
 );
-
 console.log(randomNumber);
-// Random movie
+
 async function randomMovie() {
-  const fetchRandomMovie = await fetch(
-    "https://api.themoviedb.org/3/discover/movie?api_key=7163d507a975c5833a02e7ea696637bd&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&append_to_response=watch/providers"
-  )
-    .then((response) => response.json())
-    .then((movies) => {
-      movieTitle.innerText = movies.results[randomNumber].original_title;
-      movieDescription.innerText = movies.results[randomNumber].overview;
-      movieImg.setAttribute(
-        "src",
-        `${imgLink}w300/${movies.results[randomNumber].poster_path}`
-      );
-    });
+  movieList = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&append_to_response=watch/providers`;
 
-  //Bringing where to watch:
-  const fetchWhereToWatch = fetch(
+  const fetchMovie = await fetch(movieList);
+  const movies = await fetchMovie.json();
+
+  movieTitle.innerText = movies.results[randomNumber].original_title;
+  movieDescription.innerText = movies.results[randomNumber].overview;
+  movieImg.setAttribute(
+    "src",
+    `${imgLink}w300/${movies.results[randomNumber].poster_path}`
+  );
+  const fetchProvidersList = await fetch(
     `https://api.themoviedb.org/3/movie/${randomNumber}}/watch/providers?api_key=${apiKey}`
-  )
-    .then((response) => response.json())
-    .then((providersList) => {
-      watchProviders.setAttribute(
-        "src",
-        `${imgLink}original${providersList.results.BR.flatrate[0].logo_path}`
-      );
-    });
+  );
+  try { const providersList = await fetchProvidersList.json();
+  watchProviders.setAttribute(
+    "src",
+    `${imgLink}original${providersList.results.BR.flatrate[0].logo_path}`
+  ); }
+  catch (error) {
+    const erro = document.createElement('p')
+    erro.innerText = "Não conseguimos encontrar esse filme =("
+    watchProvidersContainer.appendChild(erro)
+  }
 }
-
 randomMovie();
