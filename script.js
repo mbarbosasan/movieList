183a2ed1a90c36f2345b6831eb722d02614cfd81
moisesbarbosa23@gmail.com <moisesbarbosa23@gmail.com>
2022-02-28 17:52:34 -0300

    Adicionando novo botão "Proximo Filme"

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const movieImg = document.querySelector(".movie-img");
 const movieTitle = document.querySelector(".movie-name");
 const movieDescription = document.querySelector(".movie-description");
 const watchProviders = document.querySelector(".watch-providers");
-const watchProvidersContainer = document.querySelector(".watch-providers-container")
-const body = document.querySelector("body");
+const watchProvidersContainer = document.querySelector(
+  ".watch-providers-container"
+);
+const nextMovie = document.querySelector(".next-movie");
 
 const apiKey = `7163d507a975c5833a02e7ea696637bd`;
 const imgLink = "https://image.tmdb.org/t/p/";
 
-const maxNumber = 19;
-const minNumber = 10;
-const randomNumber = Math.floor(
-  Math.random() * (1 + maxNumber - minNumber) + minNumber
-);
-console.log(randomNumber);
-
 async function randomMovie() {
+  const maxNumber = 19;
+  const minNumber = 10;
+  const randomNumber = Math.floor(
+    Math.random() * (1 + maxNumber - minNumber) + minNumber
+  );
   movieList = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&append_to_response=watch/providers`;
 
   const fetchMovie = await fetch(movieList);
@@ -30,15 +30,23 @@ async function randomMovie() {
   const fetchProvidersList = await fetch(
     `https://api.themoviedb.org/3/movie/${randomNumber}}/watch/providers?api_key=${apiKey}`
   );
-  try { const providersList = await fetchProvidersList.json();
-  watchProviders.setAttribute(
-    "src",
-    `${imgLink}original${providersList.results.BR.flatrate[0].logo_path}`
-  ); }
-  catch (error) {
-    const erro = document.createElement('p')
-    erro.innerText = "Não conseguimos encontrar esse filme =("
-    watchProvidersContainer.appendChild(erro)
+  try {
+    const providersList = await fetchProvidersList.json();
+    watchProviders.setAttribute(
+      "src",
+      `${imgLink}original${providersList.results.BR.flatrate[0].logo_path}`
+    );
+  } catch (error) {
+    const erro = document.createElement("p");
+    erro.innerText = "Não conseguimos encontrar esse filme =(";
+    watchProvidersContainer.appendChild(erro);
   }
 }
+
+function reloadPage() {
+  document.location.reload();
+}
+
+nextMovie.addEventListener("click", reloadPage);
+
 randomMovie();
